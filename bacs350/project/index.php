2e269e5c4bc40f9451d4fbe425b667549ce3aa7c
David Thompson <thompsond873@gmail.com>
2019-12-09 16:46:04 -0700

    Update index.php

--- bacs350/project/index.php
+++ bacs350/project/index.php
@@ -1,9 +1,7 @@
 <?php
-
     /*
         Create page content by rendering a template.
     */
-
     $site_title = 'UNC BACS 350';
     
     $page_title = "My Projects";
@@ -18,16 +16,36 @@
         
         <ul>
             <li>
-                <a href="01">Project #1 - Setup Web Hosting</a>
+              <button>  <a href="../../">Setup Web Hosting</a> </button>
             </li>
             <li>
-                <a href="02">Project #2 - Page Template</a>
+               <button> <a href="02">Page Template</a>
             </li>
+            <li>
+               <button> <a href="03">Superhero Cards PHP</a> </button>
+            </li>
+            <li>
+              <button>  <a href="../subscriber">SQL Subscriber List</a> </button>
+            </li>
+             <li>
+               <button> <a href="../planner">Project #4 - Planner Sheet</a> </button>
+            </li>
+            <li>
+              <button>  <a href="../superhero">Superhero</a> </button>
+            </li>
+            <li>
+              <button>  <a href="../review">Page Review CRUD </a> </button>
+            </li>
+            <li>
+               <button> <a href="../notes">Notes App</a> </button>
+            </li>
+             <li>
+              <button>  <a href="../slides">Slideshow Creator</a> </button>
+            </li>
+            
         </ul>
     ';
-
     include '../views.php';
     
     echo render_page($site_title, $page_title, $content);
-
 ?>
