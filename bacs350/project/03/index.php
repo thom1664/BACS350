43d8b17b3434f445df397b90944c071c5a3761e3
David Thompson <thompsond873@gmail.com>
2019-12-09 17:15:21 -0700

    StockPhotos from Mitch for html cards

--- bacs350/project/03/index.php
+++ bacs350/project/03/index.php
@@ -1,63 +1,61 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-    /*
-        render_page -- build a page with custom settings
-    */
+<head>
 
-//                    
-    function render_page($site_title, $page_title, $content) {
+    <meta charset="utf-8">
+    <title>Simple HTML for Cards</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="http://shrinking-world.com/static/images/unc/favicon.ico">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Superhero Gallery</h1>
+                <h2>Card Lesson</h2>
+                 <h2>With PHP Click <a href="../../superhero/index.php"> here</a></h2>
+            </div>
+            <div class="logo col-sm-4">
+                <div class="pull-right">
+                    <img class="img-rounded img-responsive"
+                         src="https://shrinking-world.com/static/images/unc/Bear.200.png" alt="UNC Bear" width="150px">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="container-fluid">
+        <div class="row">
+           
+            <div class="card" style="width: 18rem;">
+                <img src="image1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
+            <div class="card" style="width: 18rem;">
+                <img src="image2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
         
-        return '
-            <!DOCTYPE html>
-            <html lang="en">
-                <head>
-
-                    <meta charset="UTF-8">
-                    <title>' . $page_title . '</title>
-
-                    <link rel="icon" type="image/x-icon" href="/bacs350/favicon.ico">
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-                    <link rel="stylesheet" href="/bacs350/unc.css">
-
-                </head>
-                <body>
-                
-                    <header>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-sm-8">
-                                    <h1>' . $site_title . '</h1>
-                                    <h2>' . $page_title . '</h2>
-                                </div>
-                                <div class="logo col-sm-4">
-                                    <div class="pull-right">
-                                        <img class="img-rounded img-responsive" 
-                                        src="/bacs350/images/Bear.200.png" 
-                                        alt="UNC Bear" width="150px">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </header>
-
-                    <main>
-
-                        ' . $content . '
-
-                    </main>
-                </body>
-            </html>
-        ';
-
-    }
-
-?>
-
-<?php
-    $site_title = 'UNC BACS 350';
-    $page_title = 'Superhero Gallery';
-    $content = render_card("Captain America", "Classic America hero archtype") .
-        render_card("Iron Man", "Billionaire Entrpreneur");
-    include 'views.php';
-    echo render_page($site_title, $page_title, $content);
-?>
+                <div class="card" style="width: 18rem;, height: 18rem">
+                <img src="image3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
+    </div>
+    </div>
+</main>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
